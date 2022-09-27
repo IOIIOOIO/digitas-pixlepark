d7c851b00ae5efca364c722b2e03960e6047c09e
James Wilson <jamesjw01@gmailcom>
2022-09-27 19:27:32 +0200

    chore: install nuxt, add bootstrap, configure global css and sass

---
+++ components/Tutorial.vue
@@ -0,0 +1,15 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="d-flex justify-content-center"
+  >
+    TEST
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
