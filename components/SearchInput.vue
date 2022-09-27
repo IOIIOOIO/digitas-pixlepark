91f348ec45e7799b2238e39b65b8d78178bb6284
James Wilson <jamesjw01@gmailcom>
2022-09-27 21:03:28 +0200

    feature(site): add card grid, start adding seachinput

---
+++ components/SearchInput.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>SEARCH</div>
+</template>
+
+<script>
+	export default {
+		name: "CSearchInput"
+	}
+</script>
+
+<style scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,69 @@
 <template>
-  <c-card name="Gabumon"
-          image="https://digimon.shadowsmith.com/img/agumon.jpg"
-          level="Rookie"/>
+	<div>
+		<c-search-input/>
+		<div class="o-card-grid">
+			<c-card name="Gabumon"
+					image="https://digimon.shadowsmith.com/img/agumon.jpg"
+					level="Rookie"/>
+			<c-card name="Gabumon"
+					image="https://digimon.shadowsmith.com/img/agumon.jpg"
+					level="Rookie"/>
+			<c-card name="Gabumon"
+					image="https://digimon.shadowsmith.com/img/agumon.jpg"
+					level="Rookie"/>
+			<c-card name="Gabumon"
+					image="https://digimon.shadowsmith.com/img/agumon.jpg"
+					level="Rookie"/>
+			<c-card name="Gabumon"
+					image="https://digimon.shadowsmith.com/img/agumon.jpg"
+					level="Rookie"/>
+			<c-card name="Gabumon"
+					image="https://digimon.shadowsmith.com/img/agumon.jpg"
+					level="Rookie"/>
+			<c-card name="Gabumon"
+					image="https://digimon.shadowsmith.com/img/agumon.jpg"
+					level="Rookie"/>
+			<c-card name="Gabumon"
+					image="https://digimon.shadowsmith.com/img/agumon.jpg"
+					level="Rookie"/>
+		</div>
+
+	</div>
+
 </template>
 
 <script>
-import CCard from "../components/Card";
-export default {
-  name: 'IndexPage',
-  components: {CCard},
-}
+	import CCard from "../components/Card";
+	import CSearchInput from "../components/SearchInput";
+
+	export default {
+		name: 'IndexPage',
+		components: {CSearchInput, CCard},
+	}
 </script>
+
+<style lang="scss" scoped>
+	@import "~/assets/sass/1.settings/functions/get-map-val";
+	@import "~/assets/sass/1.settings/functions/calc-rem";
+
+	@import "~/assets/sass/1.settings/bootstrap-variables";
+	@import "~bootstrap/scss/functions";
+	@import "~bootstrap/scss/variables";
+	@import "~bootstrap/scss/mixins";
+	@import "~/assets/sass/1.settings/variables/breakpoint";
+	@import "~/assets/sass/1.settings/variables/spacers";
+
+
+	.o-card-grid {
+		display: grid;
+		grid-template-columns: 1fr;
+		grid-row-gap: $spacer-17;
+		grid-column-gap: $grid-gutter-width;
+		@media #{$screen-md-up} {
+			grid-template-columns: repeat(2, 1fr);
+		}
+		@media #{$screen-lg-up} {
+			grid-template-columns: repeat(4, 1fr);
+		}
+	}
+</style>
